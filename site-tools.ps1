c9791198cb5fee8856384bd4235495594a732684
Simon Eames <simon.richard.eames@gmail.com>
2012-12-24 16:12:06 +1000

    Copy folder implemented

--- site-tools.ps1
+++ site-tools.ps1
@@ -1,21 +1,54 @@
-
 ##
 # . ./site_tools.ps1
 # sre, 2012-12-24
 #
-# ./backup_database [-Config-File CONFIG.XML  |  -server SERVER -database DATABASE  -username USR -password PWD]
-# ./backup_folder [-Config-File CONFIG.XML |-Source DIRECTORY -Destination DESTINATION ]
-# ./setup_iis [-Config-File CONFIG.XML ]
-#
-# ./backup_database -Config-File RAAF-STAGE.xml
-# ./backup_database -config-File RAAF-STAGE.xml
-# ./setup_iis -Config-File RAAF-STAGE.xml
-
-#./psake Backup-Database -Parameters { 'config-file' = 'config/RAAF-stage.xml' }
+# ./Backup-Database -Config-File CONFIG.XML
+# ./Backup-Folder   -Config-File CONFIG.XML
 
 
+Function Backup-Folder {
+[CmdletBinding()]
+Param(
+  [String] $ConfigFile
+)
+  Try {
+    Write-Verbose 'Getting Configuration'
+    $cfg = Get-Config $ConfigFile
+    $backup_dir= $cfg.Backup.site.location
+    $root_dir  = $cfg.Site.root.location
+    $files     = $cfg.Site.Include
+
+    $backup_dir= Setup-Destination-Directory $backup_dir
 
 
+    Write-Verbose "Copy File Includes"
+    $files | % {
+      $path = Join-Path $root_dir $_.path
+      if ($_.path -eq '*') {
+          Copy-Item $path $backup_dir -Recurse
+      } else {
+        if (Test-Path -PathType Leaf $path) {
+            Copy-Item -Force $path $backup_dir
+        }
+        if (Test-Path -PathType Container $path) {
+            Copy-Item -Recurse -Force $path $backup_dir
+        }
+      }
+    }
+
+    Write-Verbose "SQL Backup Command Finished"
+    Write-Host "
+  Database successfully backed up.
+
+Location:
+  Files:   $backup_dir
+
+Thanks for playing...
+"
+  } Catch [Exception] {
+    Write-Error $_.Exception.Message
+  }
+}
 
 
 
@@ -34,7 +67,7 @@ USE [[catalogue]]
         BACKUP DATABASE [[catalogue]]
         TO DISK = '[[backup]]'
             WITH FORMAT,
-              NAME = 'Full Backup of [[catalogue]] dated [[date]]'
+              NAME = 'Full Backup of [[catalogue]] to [[backup]]'
 
     COMMIT TRAN
 END TRY
@@ -51,29 +84,24 @@ GO
 
   $super_usr = $cfg.Database.user.name
   $super_pwd = $cfg.Database.user.password
-  $backup_dir= $cfg.Database.backup.dir
   $server    = $cfg.Database.connection.server
   $catalogue = $cfg.Database.connection.catalogue
   $usr       = $cfg.Database.connection.username
   $pwd       = $cfg.Database.connection.password
+  $backup_dir= $cfg.Backup.database.location
 
   $date      = Get-Date -Format yyyyMMdd
   $time      = Get-Date -Format HHmm
 
-  Write-Verbose "Setting up backup location"
-  $backup_dir= "{0}\{1}" -F $backup_dir, $date
-  New-Item $backup_dir -Type Directory -ErrorAction SilentlyContinue
-
+  $backup_dir= Setup-Destination-Directory $backup_dir
   $backup    = "{0}\{1}{2}_{3}.bak" -F $backup_dir, $date, $time, $catalogue
 
 
   Write-Verbose "Injecting Config into Database Script"
   $SQL_database_backup = $SQL_database_backup.Replace( "[[catalogue]]", $catalogue)
   $SQL_database_backup = $SQL_database_backup.Replace( "[[backup]]", $backup)
-  $SQL_database_backup = $SQL_database_backup.Replace( "[[date]]", (Get-Date -Format yyyyMMdd))
-  $SQL_database_backup = $SQL_database_backup.Replace( "[[time]]", (Get-Date -Format HHmm))
 
-  Write-Verbose "Running SQL Command"
+  Write-Verbose "Running SQL Backup Command"
   $sql_result = sqlcmd -S $server -U $super_usr -P $super_pwd -Q $SQL_database_backup -V1
   $sql_success = $?
   if (-not $sql_success ) {
@@ -83,7 +111,7 @@ GO
     Exit
   }
 
-  Write-Verbose "SQL Command Finished"
+  Write-Verbose "SQL Backup Command Finished"
   Write-Host "
 Database successfully backed up.
 
@@ -94,6 +122,20 @@ Location:
 Thanks for playing...
 "
 
+}
+
+
+Function Setup-Destination-Directory {
+[CmdletBinding()]
+Param(
+  [string]$Directory
+)
+  Write-Verbose "Setting up backup location"
+  $Directory= "{0}\{1}" -F $Directory, (Get-Date -F yyyyMMdd)
+  if (-not (Test-Path $Directory) ) {
+    New-Item $Directory -Type Directory | Out-Null
+  }
+  $Directory
 }
 
 
@@ -115,4 +157,5 @@ Param(
 }
 
 
-Backup-DataBase -ConfigFile "config/default.xml" -Verbose
+#Backup-Folder    -ConfigFile "default.xml" -Verbose
+#Backup-DataBase  -ConfigFile "default.xml" -Verbose
